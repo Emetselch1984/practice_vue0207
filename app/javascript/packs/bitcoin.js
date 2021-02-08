9a30ef6b0a1dc27835dfa3a1fedb12a2292fa3a1
Emetselch1984 <banzai.nippon2030@gmail.com>
2021-02-08 10:16:10 +0900

    Add bitcoin.js

--- app/javascript/packs/bitcoin.js
+++ app/javascript/packs/bitcoin.js
@@ -1,5 +1,27 @@
 import Vue from 'vue/dist/vue.esm'
 document.addEventListener('DOMContentLoaded', () => {
     console.log("hello")
+    var app = new Vue({
+        el: '#bitcoin',
+        data: {
+            bpi:null
+        },
+        mounted:function () {
+            axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+                .then(function (response) {
+                    // handle success
+                    console.log(response);
+                    console.log(response.data.bpi);
+                    this.bpi = response.data.bpi
+                }.bind(this))
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .then(function () {
+                    // always executed
+                });
+        }
+    })
 
 });
